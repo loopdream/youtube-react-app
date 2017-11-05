fa57f9a0794125f4484a4635f99924e0bda685c5
Loopdream <dom@loopdream.com>
2017-11-05 22:21:53 +0000

    change to procfile

--- src/server/server.js
+++ src/server/server.js
@@ -62,6 +62,6 @@ app.use('*', async (req, res) => {
 })
 
 
-const PORT = process.env.PORT || 8001
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Express is listening on port http://localhost:${PORT}`))
 
