2698610161164fc53db4195433e7df9fce4a8820
Loopdream <dom@loopdream.com>
2017-11-05 14:34:18 +0000

    syntax changes

--- src/config.js
+++ src/config.js
@@ -2,12 +2,12 @@ const config = {
   apiUrl: 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,contentDetails,status&maxResults=10&playlistId=PLSi28iDfECJPJYFA4wjlF5KUucFvc0qbQ&key=AIzaSyCuv_16onZRx3qHDStC-FUp__A6si-fStw',
   routes: [
     '/',
-    '/video/:videoId'
+    '/video/:videoId',
   ],
   pageCopy: {
     indexHeading: 'My YouTube Playlist',
-    backToListText: 'Back to list of videos'
+    backToListText: 'Back to list of videos',
   }
 }
 
-export default config;
+export default config
--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 
-import server from './server/server';
-
-const { PORT = 8001 } = process.env;
-
-server.listen(PORT, () => {
-  console.log(`Express is listening on port http://localhost:${PORT}`);
-});
+import server from './server/server'
+const { PORT = 8001 } = process.env
+server.listen(PORT, () => console.log(`Express is listening on port http://localhost:${PORT}`))
