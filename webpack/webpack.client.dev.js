13a2dc2d12c14a0b217d068d11bca2e4974205d5
Loopdream <dom@loopdream.com>
2017-11-05 21:29:26 +0000

    change

--- webpack/webpack.client.dev.js
+++ webpack/webpack.client.dev.js
@@ -2,7 +2,6 @@ const path = require('path');
 const webpack = require('webpack');
 // const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-
 module.exports = {
   context: path.join(__dirname, '../src/client'),
   devtool: 'inline-source-map',
