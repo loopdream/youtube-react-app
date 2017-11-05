f953f7142b4ea496e35efe00bd3762e64a0bfe66
Loopdream <dom@loopdream.com>
2017-11-05 21:38:59 +0000

    change to package

--- webpack/webpack.server.prod.js
+++ webpack/webpack.server.prod.js
@@ -34,11 +34,6 @@ module.exports = {
     ],
   },
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-      }
-    }),
     new UglifyJSPlugin({
       sourceMap: true
     })
