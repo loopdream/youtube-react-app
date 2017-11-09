94a6159222925c64144e19586ac0c91e163808cf
Loopdream <dom@loopdream.com>
2017-11-09 00:57:15 +0000

    added scroll to top component for nice UX

--- src/client/client.js
+++ src/client/client.js
@@ -1,12 +1,15 @@
 import 'babel-polyfill'
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './components/App'
+import ScrollToTop from './components/ScrollToTop'
 
 ReactDOM.hydrate(
   <Router>
-    <App videoData={window.__videoData__} />
+    <ScrollToTop>
+      <App videoData={window.__videoData__} />
+    </ScrollToTop>
   </Router>,
   document.getElementById('app')
 )
