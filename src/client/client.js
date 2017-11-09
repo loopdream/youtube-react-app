b53f8f6022e0c049dad11c7c6502cfaf9146af6e
Loopdream <dom@loopdream.com>
2017-11-09 00:37:55 +0000

    changes to package

--- src/client/client.js
+++ src/client/client.js
@@ -4,8 +4,6 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './components/App'
 
-
-
 ReactDOM.hydrate(
   <Router>
     <App videoData={window.__videoData__} />
