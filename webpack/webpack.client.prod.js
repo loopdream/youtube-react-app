442db23b0f2d6b6de558fbfba74024ec737caefc
Loopdream <dom@loopdream.com>
2017-11-11 13:45:59 +0000

    package refactor - heroku env is set to prod so  moved lots of libraries out of devDependencies and into dependencies so heroku can build, added node_env production to webpack client

--- webpack/webpack.client.prod.js
+++ webpack/webpack.client.prod.js
@@ -35,6 +35,11 @@ module.exports = {
     ]
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
     new UglifyJSPlugin({
       sourceMap: true
     })
